ccc522da3a718a9462ec45d576bd61461b4d3f51
JMTE <JMTEdeveloper@outlook.es>
2021-07-21 14:26:28 +0200

    Se añaden los cambios y se crea el componente heroe

--- 01-Bases/src/app/app.module.ts
+++ 01-Bases/src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador.component';
+import { ContadorComponent } from './contador/contador.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
 
 @NgModule({
   declarations: [
-    AppComponent, ContadorComponent
+    AppComponent, ContadorComponent, HeroeComponent
   ],
   imports: [
     BrowserModule
