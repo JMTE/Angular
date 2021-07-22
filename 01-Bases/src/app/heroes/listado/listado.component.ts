f9b3ff8ee1d130478524a2952323d49d669334b1
JMTE <JMTEdeveloper@outlook.es>
2021-07-22 13:21:04 +0200

    "Ejemplo de directiva ngFor"

--- 01-Bases/src/app/heroes/listado/listado.component.ts
+++ 01-Bases/src/app/heroes/listado/listado.component.ts
@@ -1,12 +1,38 @@
 import { Component} from '@angular/core';
+import { resetFakeAsyncZone } from '@angular/core/testing';
+import { VirtualAction } from 'rxjs';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   
 })
-export class ListadoComponent { //ciclos de vida
+export class ListadoComponent { 
+
+  heroes:string[]=["Spiderman","Hulk","Batman","Ironman","Joker","Thor"];
+
+  heroesBorrados:string[]=[];
+
+  public borrarHeroe(){
+
+
+
+     const heroeBorrado=this.heroes.shift() ||""; 
+     // Esto es debido a que shift devuelve un string o undefined, por lo tanto a la hora de 
+    //  declarar el heroes Borrados habria que declararlo como any y eso no es muy conveniente,
+    //  por esa razon se pone que si es undefined devuelva un string Vacio
+
+    
+
+    
+    this.heroesBorrados.push(heroeBorrado);
+    
+
+   
+
+  }
 
   
-
 }
+
+
