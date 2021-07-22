842c14f6d373fd94d5f2ec469eafbf6e206a1350
JMTE <JMTEdeveloper@outlook.es>
2021-07-22 12:25:32 +0200

    "Se crea un nuevo componente de forma automatica"

---
+++ 01-Bases/src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,12 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  
+})
+export class ListadoComponent { //ciclos de vida
+
+  
+
+}
