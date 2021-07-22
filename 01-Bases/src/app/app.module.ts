ee7052f32876921823229521189c4a49d9ad1bb8
JMTE <JMTEdeveloper@outlook.es>
2021-07-22 13:50:02 +0200

    "Introduccion a separar en modulos diferentes"

--- 01-Bases/src/app/app.module.ts
+++ 01-Bases/src/app/app.module.ts
@@ -3,15 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { ListadoComponent } from './heroes/listado/listado.component';
+import { HeroesModule } from './heroes/heroes.modulo';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent, ContadorComponent, HeroeComponent, ListadoComponent
+    AppComponent, ContadorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
