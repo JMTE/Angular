32329acf8218501d77262e48c672bf40802dcce9
JMTE <JMTEdeveloper@outlook.es>
2021-08-06 13:02:52 +0200

    Utilizamos el nodo ViewChield mediante el cual podemos guardar el valor de la caja de texto y despues borrar esa misma caja de texto.

--- 02-appGifs/src/app/gifs/zona-gifs/search/search.component.ts
+++ 02-appGifs/src/app/gifs/zona-gifs/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -7,6 +7,22 @@ import { Component } from '@angular/core';
 export class SearchComponent {
 
   constructor() { }
+
+  @ViewChild("txtBuscar") txtBuscar!:ElementRef<HTMLInputElement>;
+
+  //! con este signo le decimos que este elemento nunva va a ser nulo
+
+  buscar(){
+
+
+    const  valor=  (this.txtBuscar.nativeElement.value);
+
+    console.log(valor);
+
+    this.txtBuscar.nativeElement.value="";
+    
+    
+  }
 
   
 
